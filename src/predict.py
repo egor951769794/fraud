e80d942cd5dfc69935b9c887ae3e0fdcb424a085
1asa9na <rage011111@gmail.com>
2024-07-13 06:31:23 +0600

    Revert "Merge pull request #1 from egor951769794/feature/1/implement_model"
    This reverts commit eba95f4e36d66b3fa325964d711bac4157698d06, reversing
    changes made to 106f5940a8beb5021e7df695eb03928fbac22851.

--- src/predict.py
+++
@@ -1,9 +0,0 @@
-import pickle as pkl
-
-class Prediction:
-    def __init__(self, filename):
-        self.model = pkl.load(open(filename, 'rb'))
-
-    def predict(self, X):
-        X_clean = X[['time_diff_seconds_std', 'amount_std', 'address_Kfold_Target_Enc', 'is_passport_expired', 'same_passport', 'same_phone', 'operation_type_Kfold_Target_Enc', 'terminal_type_Kfold_Target_Enc']]
-        return self.model.predict(X_clean)
